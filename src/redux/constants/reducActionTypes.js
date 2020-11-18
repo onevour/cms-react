283292f83c7769f736829531006b7f65eaae53f2
rebelrepo <zuliadin@rebelworks.co>
2020-11-18 14:05:20 +0700

    upload

--- src/redux/constants/reducActionTypes.js
+++ src/redux/constants/reducActionTypes.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "http://5.181.217.229:8081";
-export const BASE_URL = "http://localhost:8081/api/v1";
+export const BASE_URL = "http://5.181.217.229:8081/api/v1";
+// export const BASE_URL = "http://localhost:8081/api/v1";
 
 export const POST = 'POST'
 export const GET = 'GET'
